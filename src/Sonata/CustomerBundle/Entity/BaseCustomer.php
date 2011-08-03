cbc2466e466ceef1cd7167cb612cf607d309090c
Rémi Marseille <marseille@ekino.com>
2011-08-03 16:01:58 +0200

    Add customer properties (title, email, birthDate, birthPlace, phoneNumber, mobileNumber and faxNumber)

--- src/Sonata/CustomerBundle/Entity/BaseCustomer.php
+++ src/Sonata/CustomerBundle/Entity/BaseCustomer.php
@@ -3,12 +3,22 @@
 namespace Sonata\CustomerBundle\Entity;
 
 use Sonata\Component\Customer\CustomerInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use Sonata\Component\Customer\AddressInterface;
 
 /**
- * Sonata\BasketBundle\Entity\BaseAddress
+ * Sonata\CustomerBundle\Entity\BaseCustomer
  */
 abstract class BaseCustomer implements CustomerInterface
 {
+    const TITLE_MLLE = 1;
+    const TITLE_MME = 2;
+    const TITLE_MR = 3;
+
+    /**
+     * @var string $title
+     */
+    protected $title;
 
     /**
      * @var string $firstname
@@ -20,6 +30,35 @@ abstract class BaseCustomer implements CustomerInterface
      */
     protected $lastname;
 
+    /**
+     * @var string $email
+     */
+    protected $email;
+
+    /**
+     * @var string $birthDate
+     */
+    protected $birthDate;
+
+    /**
+     * @var string $birthPlace
+     */
+    protected $birthPlace;
+
+    /**
+     * @var string $phoneNumber
+     */
+    protected $phoneNumber;
+
+    /**
+     * @var string $mobileNumber
+     */
+    protected $mobileNumber;
+
+    /**
+     * @var string $faxNumber
+     */
+    protected $faxNumber;
 
     /**
      * @var datetime $updatedAt
@@ -36,20 +75,46 @@ abstract class BaseCustomer implements CustomerInterface
      */
     protected $user;
 
+    /**
+     * @var Collection
+     */
+    protected $addresses;
 
-    public function setCreatedAt(\DateTime $createdAt = null)
+    public function __construct()
     {
-        $this->createdAt = $createdAt;
+        $this->title = self::TITLE_MR;
+        $this->addresses = new ArrayCollection();
     }
 
-    public function getCreatedAt()
+    public function __toString()
     {
-        return $this->createdAt;
+        return $this->getFirstname().' '.$this->getLastname();
     }
 
-    public function setFirstname($firstname)
+    public function getTitle()
     {
-        $this->firstname = $firstname;
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public static function getTitlesList()
+    {
+        return array(
+            self::TITLE_MLLE => 'sonata_customer_title_mlle',
+            self::TITLE_MME => 'sonata_customer_title_mme',
+            self::TITLE_MR => 'sonata_customer_title_mr',
+        );
+    }
+
+    public function getTitleName()
+    {
+        $list = self::getTitlesList();
+
+        return isset($list[$this->title]) ? $list[$this->title] : '';
     }
 
     public function getFirstname()
@@ -57,13 +122,9 @@ abstract class BaseCustomer implements CustomerInterface
         return $this->firstname;
     }
 
-    public function getFullname()
+    public function setFirstname($firstname)
     {
-        return $this->getFirstname(). ' ' . $this->getLastname();
-    }
-    public function setLastname($lastname)
-    {
-        $this->lastname = $lastname;
+        $this->firstname = $firstname;
     }
 
     public function getLastname()
@@ -71,14 +132,94 @@ abstract class BaseCustomer implements CustomerInterface
         return $this->lastname;
     }
 
-    public function setUpdatedAt(\DateTime $updatedAt = null)
+    public function setLastname($lastname)
     {
-        $this->updatedAt = $updatedAt;
+        $this->lastname = $lastname;
+    }
+
+    public function getFullname()
+    {
+        return $this->getFirstname(). ' ' . $this->getLastname();
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getBirthDate()
+    {
+        return $this->birthDate;
+    }
+
+    public function setBirthDate($birthDate)
+    {
+        $this->birthDate = $birthDate;
+    }
+
+    public function getBirthPlace()
+    {
+        return $this->birthPlace;
+    }
+
+    public function setBirthPlace($birthPlace)
+    {
+        $this->birthPlace = $birthPlace;
+    }
+
+    public function getPhoneNumber()
+    {
+        return $this->phoneNumber;
+    }
+
+    public function setPhoneNumber($phoneNumber)
+    {
+        $this->phoneNumber = $phoneNumber;
+    }
+
+    public function getMobileNumber()
+    {
+        return $this->mobileNumber;
+    }
+
+    public function setMobileNumber($mobileNumber)
+    {
+        $this->mobileNumber = $mobileNumber;
+    }
+
+    public function getFaxNumber()
+    {
+        return $this->faxNumber;
+    }
+
+    public function setFaxNumber($faxNumber)
+    {
+        $this->faxNumber = $faxNumber;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt = null)
+    {
+        $this->createdAt = $createdAt;
     }
 
     public function getUpdatedAt()
     {
         return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTime $updatedAt = null)
+    {
+        $this->updatedAt = $updatedAt;
     }
 
     public function setUser($user)
@@ -91,8 +232,26 @@ abstract class BaseCustomer implements CustomerInterface
         return $this->user;
     }
 
-    public function __toString()
+    public function addAddress(AddressInterface $address)
     {
-        return $this->getFirstname().' '.$this->getLastname();
+        $this->addresses[] = $address;
+    }
+
+    public function getAddresses()
+    {
+        return $this->addresses;
+    }
+
+    public function getAddressesByType($type)
+    {
+        $addresses = array();
+
+        foreach ($this->getAddresses() as $address) {
+            if ($type == $address->getType()) {
+                $addresses[] = $address;
+            }
+        }
+
+        return $addresses;
     }
 }
