9b8da8b440e285988ec2d65e165a510be86e2ea8
Rémi Marseille <marseille@ekino.com>
2011-08-11 16:27:41 +0200

    Rename type property in product_type

--- src/Sonata/Component/Subscriber/ORMInheritanceSubscriber.php
+++ src/Sonata/Component/Subscriber/ORMInheritanceSubscriber.php
@@ -37,7 +37,7 @@ class ORMInheritanceSubscriber implements EventSubscriber
             return;
         }
 
-        $metadata->setDiscriminatorColumn(array('name' => 'type', 'type' => 'string', 'length' => 64));
+        $metadata->setDiscriminatorColumn(array('name' => 'product_type', 'type' => 'string', 'length' => 64));
         $metadata->setInheritanceType(ClassMetadataInfo::INHERITANCE_TYPE_SINGLE_TABLE);
         $metadata->setDiscriminatorMap($this->map);
     }
