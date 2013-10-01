d2cc8f86599982521b572a80ab5e05f12fc56775
Xavier Coureau <xav.is@2cool4school.fr>
2013-10-01 16:17:55 +0200

    SONATA-80 - Added tests for MysqlReference class

---
+++ tests/Sonata/Tests/Component/Generator/MysqlReferenceTest.php
@@ -0,0 +1,129 @@
+<?php
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\Test\Component\Generator;
+
+use Doctrine\ORM\EntityManager as BaseEntityManager;
+
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Sonata\Component\Generator\MysqlReference;
+
+use Application\Sonata\InvoiceBundle\Entity\Invoice;
+use Application\Sonata\OrderBundle\Entity\Order;
+
+class EntityManager extends BaseEntityManager
+{
+    public function getClassMetadata($className)
+    {
+        $obj = new \stdClass();
+        $obj->table['name'] = 'test';
+
+        return $obj;
+    }
+}
+
+class InvoiceMock extends Invoice
+{
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+}
+
+class OrderMock extends Order
+{
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+}
+
+/**
+ * Class MysqlReferenceTest
+ *
+ * @package Sonata\Test\OrderBundle\Entity
+ *
+ * @author Xavier Coureau <xcoureau@ekino.com>
+ */
+class MysqlReferenceTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInvoice()
+    {
+        $invoice = new InvoiceMock();
+        $mysqlReference = $this->generateNewObject();
+
+        try {
+            $mysqlReference->invoice($invoice);
+            $this->fail('->invoice() call should raise a \RuntimeException for a new entity');
+        }
+        catch (\Exception $e) {
+            $this->assertInstanceOf('\RuntimeException', $e);
+        }
+
+        $invoice->setId(12);
+
+        try {
+            $this->assertNull($mysqlReference->invoice($invoice));
+        }
+        catch (\Exception $e) {
+            $this->fail('->invoice() should return a NULL value but should now throw an \Exception');
+        }
+    }
+
+    public function testOrder()
+    {
+        $order = new OrderMock();
+        $mysqlReference = $this->generateNewObject();
+
+        try {
+            $mysqlReference->order($order);
+            $this->fail('->order() call should raise a \RuntimeException for a new entity');
+        }
+        catch (\Exception $e) {
+            $this->assertInstanceOf('\RuntimeException', $e);
+        }
+
+        $order->setId(12);
+
+        try {
+            $this->assertNull($mysqlReference->order($order));
+        }
+        catch (\Exception $e)
+        {
+            $this->fail('->order() should return a NULL value but should not throw an \Exception');
+        }
+    }
+
+    /**
+     * @return \Sonata\Component\Generator\MysqlReference
+     */
+    private function generateNewObject()
+    {
+        $metadata = new ClassMetadata('entityName');
+        $metadata->table = array('name' => 'tableName');
+
+        $connection = $this->getMockBuilder('Doctrine\DBAL\Connection')->disableOriginalConstructor()->getMock();
+
+        $em = $this->getMockBuilder('Doctrine\ORM\EntityManager')->disableOriginalConstructor()->getMock();
+        $em->expects($this->any())
+            ->method('getClassMetadata')
+            ->will($this->returnValue($metadata));
+
+        $em->expects($this->any())
+            ->method('getConnection')
+            ->will($this->returnValue($connection));
+
+        $connection->expects($this->any())
+            ->method('query')
+            ->will($this->returnValue(new \PDOStatement()));
+
+        return new MysqlReference($em);
+    }
+}
