243c210a00c35cc7afaf0d247356ea1b20d98297
Xavier Coureau <xav.is@2cool4school.fr>
2013-10-02 15:27:23 +0200

    SONATA-80 - Added class tests

---
+++ tests/Sonata/Tests/Component/Payment/SelectorTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\Tests\Component\Payment;
+
+use Sonata\Component\Payment\Selector;
+use Sonata\CustomerBundle\Entity\BaseAddress;
+
+class Address extends BaseAddress
+{
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
+/**
+ * Class SelectorTest
+ *
+ * @package Sonata\Test\Component\Payment
+ *
+ * @author Xavier Coureau <xcoureau@ekino.com>
+ */
+class SelectorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetPaymentPool()
+    {
+        $paymentPoolMethods = array('first method', 'second method');
+
+        $paymentPool = $this->getMockBuilder('Sonata\Component\Payment\Pool')->getMock();
+        $paymentPool->expects($this->any())
+            ->method('getMethods')
+            ->will($this->returnValue($paymentPoolMethods));
+
+        $productPool = $this->getMockBuilder('Sonata\Component\Product\Pool')->getMock();
+
+        $selector = new Selector($paymentPool, $productPool);
+        $this->assertFalse($selector->getAvailableMethods());
+        $this->assertEquals($paymentPoolMethods, $selector->getAvailableMethods(null, new Address()));
+    }
+}
