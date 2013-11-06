37648c448e6b4dcc37aa349ba7e452fe142658d7
Sylvain Deloux <sylvain.deloux@fullsix.com>
2013-11-06 16:47:44 +0100

    SONATA-134 - Added a condition to view the Product

--- src/Sonata/ProductBundle/Controller/ProductController.php
+++ src/Sonata/ProductBundle/Controller/ProductController.php
@@ -30,7 +30,7 @@ class ProductController extends Controller
      */
     public function viewAction($productId, $slug)
     {
-        $product = is_object($productId) ? $productId : $this->get('sonata.product.set.manager')->findOneBy(array('id' =>  $productId));
+        $product = is_object($productId) ? $productId : $this->get('sonata.product.set.manager')->findOneBy(array('id' =>  $productId, 'enabled' => true));
 
         if (!$product) {
             throw new NotFoundHttpException(sprintf('Unable to find the product with id=%d', $productId));
