0f3bf60c90f446ef5c381ace8853d14a10734191
Hugo Briand <briand@ekino.com>
2013-12-11 15:24:01 +0100

    SONATA-291 - Changing getImage algorithm in order to return first media by position

--- tests/Sonata/Tests/ProductBundle/Entity/BaseProductTest.php
+++ tests/Sonata/Tests/ProductBundle/Entity/BaseProductTest.php
@@ -56,17 +56,34 @@ class BaseProductTest extends \PHPUnit_Framework_TestCase
         $media = $this->getMock('Sonata\MediaBundle\Model\MediaInterface');
 
         $galleryHasMedia = $this->getMock('Sonata\MediaBundle\Model\GalleryHasMediaInterface');
-        $galleryHasMedia->expects($this->once())
+        $galleryHasMedia->expects($this->exactly(2))
             ->method('getMedia')
             ->will($this->returnValue($media));
+        $galleryHasMedia->expects($this->exactly(2))
+            ->method('getPosition')
+            ->will($this->returnValue(10));
+
+        $galleryHasMedia2 = $this->getMock('Sonata\MediaBundle\Model\GalleryHasMediaInterface');
+
+        $media->expects($this->once())
+            ->method('getName')
+            ->will($this->returnValue("correctMedia"));
+
+        $galleryHasMedia2->expects($this->exactly(2))
+            ->method('getMedia')
+            ->will($this->returnValue($media));
+        $galleryHasMedia2->expects($this->exactly(2))
+            ->method('getPosition')
+            ->will($this->returnValue(2));
 
         $gallery = $this->getMock('Sonata\MediaBundle\Model\GalleryInterface');
-        $gallery->expects($this->once())
+        $gallery->expects($this->exactly(2))
             ->method('getGalleryHasMedias')
-            ->will($this->returnValue(array($galleryHasMedia)));
+            ->will($this->returnValue(array($galleryHasMedia, $galleryHasMedia2)));
 
         $product->setGallery($gallery);
 
         $this->assertInstanceOf('Sonata\MediaBundle\Model\MediaInterface', $product->getImage());
+        $this->assertEquals("correctMedia", $product->getImage()->getName());
     }
 }
