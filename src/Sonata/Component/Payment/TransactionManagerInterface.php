98613fd28d5ff0491122ed472ac24e6cbdaa24c5
Sylvain Deloux <sylvain.deloux@fullsix.com>
2013-12-12 18:22:25 +0100

    SONATA-261 - Added DoctrineBaseManager to PaymentBundle

--- src/Sonata/Component/Payment/TransactionManagerInterface.php
+++ src/Sonata/Component/Payment/TransactionManagerInterface.php
@@ -11,51 +11,8 @@
 
 namespace Sonata\Component\Payment;
 
-interface TransactionManagerInterface
+use Sonata\CoreBundle\Entity\ManagerInterface;
+
+interface TransactionManagerInterface extends ManagerInterface
 {
-    /**
-     * Creates an empty transaction instance
-     *
-     * @return TransactionInterface
-     */
-    public function create();
-
-    /**
-     * Deletes a transaction
-     *
-     * @param  TransactionInterface $transaction
-     * @return void
-     */
-    public function delete(TransactionInterface $transaction);
-
-    /**
-     * Finds one transaction by the given criteria
-     *
-     * @param  array                $criteria
-     * @return TransactionInterface
-     */
-    public function findOneBy(array $criteria);
-
-    /**
-     * Finds many transaction by the given criteria
-     *
-     * @param  array                $criteria
-     * @return TransactionInterface
-     */
-    public function findBy(array $criteria);
-
-    /**
-     * Returns the transaction's fully qualified class name
-     *
-     * @return string
-     */
-    public function getClass();
-
-    /**
-     * Updates a transaction
-     *
-     * @param  TransactionInterface $transaction
-     * @return void
-     */
-    public function save(TransactionInterface $transaction);
 }
