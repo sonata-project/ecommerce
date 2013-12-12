a0e6fe356c38062653d682d8083e6c001d584ab3
Sylvain Deloux <sylvain.deloux@fullsix.com>
2013-12-12 15:50:52 +0100

    SONATA-261 - Added DoctrineBaseManager to InvoiceBundle

--- src/Sonata/Component/Invoice/InvoiceElementManagerInterface.php
+++ src/Sonata/Component/Invoice/InvoiceElementManagerInterface.php
@@ -11,51 +11,8 @@
 
 namespace Sonata\Component\Invoice;
 
-interface InvoiceElementManagerInterface
+use Sonata\CoreBundle\Entity\ManagerInterface;
+
+interface InvoiceElementManagerInterface extends ManagerInterface
 {
-    /**
-     * Creates an empty invoiceElement instance
-     *
-     * @return InvoiceElementInterface
-     */
-    public function create();
-
-    /**
-     * Deletes a invoiceElement
-     *
-     * @param  InvoiceElementInterface $invoiceElement
-     * @return void
-     */
-    public function delete(InvoiceElementInterface $invoiceElement);
-
-    /**
-     * Finds one invoiceElement by the given criteria
-     *
-     * @param  array                   $criteria
-     * @return InvoiceElementInterface
-     */
-    public function findOneBy(array $criteria);
-
-    /**
-     * Finds one invoiceElement by the given criteria
-     *
-     * @param  array                   $criteria
-     * @return InvoiceElementInterface
-     */
-    public function findBy(array $criteria);
-
-    /**
-     * Returns the invoiceElement's fully qualified class name
-     *
-     * @return string
-     */
-    public function getClass();
-
-    /**
-     * Updates a invoiceElement
-     *
-     * @param  InvoiceElementInterface $invoiceElement
-     * @return void
-     */
-    public function save(InvoiceElementInterface $invoiceElement);
 }
