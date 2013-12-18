66075ae7375daca756e498a0a736af0a90c098f4
Xavier Coureau <xav.is@2cool4school.fr>
2013-12-18 17:27:01 +0100

    Merge related to PR #22

--- src/Sonata/ProductBundle/Twig/Extension/ProductExtension.php
+++ src/Sonata/ProductBundle/Twig/Extension/ProductExtension.php
@@ -14,6 +14,9 @@ namespace Sonata\ProductBundle\Twig\Extension;
 use Sonata\Component\Product\Pool as ProductPool;
 use Sonata\Component\Product\ProductInterface;
 use Sonata\Component\Product\ProductProviderInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Sonata\IntlBundle\Templating\Helper\DateTimeHelper;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 
 /**
  * @author Sylvain Deloux <sylvain.deloux@fullsix.com>
@@ -26,11 +29,23 @@ class ProductExtension extends \Twig_Extension
     protected $productPool;
 
     /**
+     * @var RouterInterface
+     */
+    protected $router;
+
+    /**
+     * @var DateTimeHelper
+     */
+    protected $dateTimeHelper;
+
+    /**
      * Constructor.
      *
      * @param ProductPool $productPool
+     * @param RouterInterface $router
+     * @param DateTimeHelper $dateTimeHelper
      */
-    public function __construct(ProductPool $productPool)
+    public function __construct(ProductPool $productPool, RouterInterface $router, DateTimeHelper $dateTimeHelper)
     {
         $this->productPool = $productPool;
     }
@@ -46,6 +61,7 @@ class ProductExtension extends \Twig_Extension
             new \Twig_SimpleFunction('sonata_product_has_enabled_variations', array($this, 'hasEnabledVariations')),
             new \Twig_SimpleFunction('sonata_product_cheapest_variation',     array($this, 'getCheapestEnabledVariation')),
             new \Twig_SimpleFunction('sonata_product_price',                  array($this, 'getProductPrice')),
+            new \Twig_SimpleFunction('sonata_product_jstree',                 array($this, 'jsonTreeBuilder')),
         );
     }
 
@@ -120,5 +136,68 @@ class ProductExtension extends \Twig_Extension
     public function getName()
     {
         return 'sonata_product';
+    }
+
+    /**
+     * @param ProductInterface         $product
+     *
+     * @return string
+     */
+    public function jsonTreeBuilder(ProductInterface $product)
+    {
+        $provider = $this->getProductProvider($product);
+
+        if (!$provider->hasEnabledVariations($product)) {
+            return '{}';
+        }
+
+        $return = array();
+        $variations = $product->getVariations();
+        $nbItems = count($variations);
+        $previousPropertyName = null;
+        $accessor = PropertyAccess::createPropertyAccessor();
+
+        foreach ($provider->getVariationFields($product) as $field) {
+            for ($j = 0; $j < $nbItems; ++$j) {
+                $variation = $variations[$j];
+
+                $value = $accessor->getValue($variation, $field);
+                $value = $this->formatTimestampProperty($value);
+
+                if (!isset($return[$field][$value])) {
+                    $return[$field][$value]['uri'] = array();
+                }
+
+                $productUri = $this->router->generate('sonata_product_view', array(
+                        'productId' => $variation->getId(),
+                        'slug'      => $variation->getSlug(),
+                    ));
+                $return[$field][$value]['uri'][] = $productUri;
+
+                if ($previousPropertyName) {
+                    $previousValue = $accessor->getValue($variation, $previousPropertyName);
+                    $previousValue = $this->formatTimestampProperty($previousValue);
+                    $return[$previousPropertyName][$previousValue][$field] = &$return[$field][$value];
+                }
+            }
+
+            $previousPropertyName = $field;
+        }
+
+        return json_encode($return);
+    }
+
+    /**
+     * @param mixed $input
+     *
+     * @return string
+     */
+    protected function formatTimestampProperty($input)
+    {
+        if ($input instanceof \DateTime) {
+            $input = $this->dateTimeHelper->formatDateTime($input);
+        }
+
+        return $input;
     }
 }
