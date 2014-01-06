b1646da90c6bac8e7c52090b77cf007ffff48366
Sylvain Deloux <sylvain.deloux@gmail.com>
2014-01-06 15:20:40 +0000

    Added DebugPayment

---
+++ src/Sonata/Component/Payment/Debug/DebugPayment.php
@@ -0,0 +1,113 @@
+<?php
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\Component\Payment\Debug;
+
+use Sonata\Component\Order\OrderInterface;
+use Sonata\Component\Payment\PassPayment;
+use Sonata\Component\Payment\TransactionInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Implement debug payment.
+ *
+ * @author Sylvain Deloux <sylvain.deloux@fullsix.com>
+ */
+class DebugPayment extends PassPayment
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function sendbank(OrderInterface $order)
+    {
+        return new RedirectResponse($this->router->generate('sonata_payment_debug', array(
+            'check'     => $this->generateUrlCheck($order),
+            'reference' => $order->getReference(),
+        )));
+    }
+
+    /**
+     * Handle the callback.
+     *
+     * @param OrderInterface $order
+     * @param string         $action
+     *
+     * @return Response
+     */
+    public function processCallback(OrderInterface $order, $action)
+    {
+        $params = array(
+            'bank'      => $this->getCode(),
+            'reference' => $order->getReference(),
+            'check'     => $this->generateUrlCheck($order),
+            'action'    => $action,
+        );
+
+        $url = $this->router->generate($this->getOption('url_callback'), $params, true);
+
+        $response = $this->browser->get($url);
+
+        $routeName = 'ok' === $response->getContent() ? 'url_return_ok' : 'url_return_ko';
+
+        $response = new RedirectResponse($this->router->generate($this->getOption($routeName), $params, true), 302, array(
+            'Content-Type' => 'text/plain',
+        ));
+        $response->setPrivate();
+
+        return $response;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function sendConfirmationReceipt(TransactionInterface $transaction)
+    {
+        $parameters = $transaction->getParameters();
+
+        if (!array_key_exists('action', $parameters)) {
+            throw new \RuntimeException('"action" parameter is missing from Transaction.');
+        }
+
+        switch ($parameters['action']) {
+            case 'accept':
+                $transaction->setState(TransactionInterface::STATE_OK);
+                $transaction->setStatusCode(TransactionInterface::STATUS_VALIDATED);
+
+                $transaction->getOrder()->setValidatedAt(new \DateTime);
+                $transaction->getOrder()->setStatus(OrderInterface::STATUS_VALIDATED);
+                $transaction->getOrder()->setPaymentStatus(TransactionInterface::STATUS_VALIDATED);
+
+                return new Response('ok', 200, array(
+                    'Content-Type' => 'text/plain',
+                ));
+
+            case 'refuse':
+                $transaction->setState(TransactionInterface::STATE_KO);
+                $transaction->setStatusCode(TransactionInterface::STATUS_ERROR_VALIDATION);
+
+                return false;
+
+            default:
+                $transaction->setState(TransactionInterface::STATE_KO);
+                $transaction->setStatusCode(TransactionInterface::STATUS_ERROR_VALIDATION);
+
+                return false;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isRequestValid(TransactionInterface $transaction)
+    {
+        return $transaction->get('check') === $this->generateUrlCheck($transaction->getOrder());
+    }
+}
---
+++ src/Sonata/PaymentBundle/Controller/DebugPaymentController.php
@@ -0,0 +1,115 @@
+<?php
+
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\PaymentBundle\Controller;
+
+use Sonata\Component\Order\OrderInterface;
+use Sonata\Component\Payment\InvalidTransactionException;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+/**
+ * Class DebugPaymentController
+ *
+ * @author Sylvain Deloux <sylvain.deloux@fullsix.com>
+ */
+class DebugPaymentController extends Controller
+{
+    /**
+     * User choice action.
+     *
+     * @return Response
+     */
+    public function paymentAction()
+    {
+        $order = $this->checkRequest();
+
+        return $this->render('SonataPaymentBundle:Payment:debug.html.twig', array(
+            'order' => $order,
+            'check' => $this->getRequest()->get('check'),
+        ));
+    }
+
+    /**
+     * Process the User choice.
+     *
+     * @return Reponse
+     */
+    public function processPaymentAction()
+    {
+        $order = $this->checkRequest();
+
+        return $this->getDebugPayment()->processCallback($order, $this->getRequest()->get('action'));
+    }
+
+    /**
+     * Check the Request and return the current Order.
+     *
+     * @return OrderInterface
+     *
+     * @throws InvalidTransactionException
+     * @throws NotFoundHttpException
+     * @throws \RuntimeException
+     */
+    protected function checkRequest()
+    {
+        if ('prod' === $this->getKernel()->getEnvironment()) {
+            throw new \RuntimeException('Debug Payment is not authorized in production environment.');
+        }
+
+        $reference = $this->getRequest()->get('reference');
+
+        $order = $this->getOrderManager()->findOneBy(array('reference' => $reference));
+
+        if (!$order) {
+            throw new NotFoundHttpException(sprintf('Order with reference "%s" not found.', $reference));
+        }
+
+        if ($this->getRequest()->get('check') !== $this->getDebugPayment()->generateUrlCheck($order)) {
+            throw new InvalidTransactionException($reference);
+        }
+
+        return $order;
+    }
+
+    /**
+     * @return \Sonata\Component\Payment\Debug\DebugPayment
+     */
+    protected function getDebugPayment()
+    {
+        return $this->get('sonata.payment.method.debug');
+    }
+
+    /**
+     * @return \Sonata\OrderBundle\Entity\OrderManager
+     */
+    protected function getOrderManager()
+    {
+        return $this->get('sonata.order.manager');
+    }
+
+    /**
+     * @return \Symfony\Component\HttpKernel\Kernel
+     */
+    protected function getKernel()
+    {
+        return $this->get('kernel');
+    }
+
+    /**
+     * @return \Symfony\Component\Routing\RouterInterface
+     */
+    protected function getRouter()
+    {
+        return $this->get('router');
+    }
+}
