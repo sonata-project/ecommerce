8730a1b8fd4867c27fd53683cab7edd485a6a8f3
Sylvain Deloux <sylvain.deloux@gmail.com>
2014-01-06 15:20:40 +0000

    Added DebugPayment

---
+++ docs/reference/bundles/payment/debug.rst
@@ -0,0 +1,40 @@
+.. index::
+single: Payment; Debug
+
+===================
+Debug payment setup
+===================
+
+Presentation
+============
+
+Debug Payment simulates the bank for your testing purpose. The ``sendbank`` will redirect you to a page where you can act as the bank and choose the action you want (accept or refuse the payment).
+
+Then, ``DebugPayment`` will call itself over HTTP (it needs to be self-reachable through its URL) and process the payment or not.
+
+For security reasons, this method is only available in dev environment.
+
+Configuration
+=============
+
+.. code-block:: yaml
+
+    sonata_payment:
+        # ...
+        services:
+            # Which payment methods are enabled?
+            # ...
+            debug:
+                name:    Debug Payment
+                enabled: true
+                code:    debug
+                browser: sonata.payment.browser.curl
+
+                transformers:
+                    basket: sonata.payment.transformer.basket
+                    order:  sonata.payment.transformer.order
+
+                options:
+                    url_callback:  sonata_payment_callback
+                    url_return_ko: sonata_payment_error
+                    url_return_ok: sonata_payment_confirmation
---
+++ tests/Sonata/Tests/Component/Payment/DebugPaymentTest.php
@@ -0,0 +1,102 @@
+<?php
+
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\Tests\Component\Payment;
+
+use Application\Sonata\PaymentBundle\Entity\Transaction;
+use Sonata\Component\Payment\Debug\DebugPayment;
+use Buzz\Browser;
+use Sonata\Component\Payment\TransactionInterface;
+use Sonata\OrderBundle\Entity\BaseOrder;
+
+class DebugPaymentTest_Order extends BaseOrder
+{
+    public function getId()
+    {
+    }
+}
+
+class DebugPaymentTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDebugPayment()
+    {
+        $payment = $this->getDebugPayment();
+
+        $order = $this->getOrder();
+
+        $transaction = $this->getTransactionManager()->create();
+        $transaction->setPaymentCode($payment->getCode());
+        $transaction->setOrder($order);
+
+        /*
+         * Payment refused
+         */
+        $transaction->setParameters(array('action' => 'refuse'));
+        $payment->sendConfirmationReceipt($transaction);
+
+        $this->assertEquals(TransactionInterface::STATE_KO, $transaction->getState());
+        $this->assertEquals(TransactionInterface::STATUS_ERROR_VALIDATION, $transaction->getStatusCode());
+
+        /*
+         * Payment accepted
+         */
+        $transaction->setParameters(array('action' => 'accept'));
+        $payment->sendConfirmationReceipt($transaction);
+
+        $this->assertEquals(TransactionInterface::STATE_OK, $transaction->getState());
+        $this->assertEquals(TransactionInterface::STATUS_VALIDATED, $transaction->getStatusCode());
+    }
+
+    /**
+     * @return \Sonata\Component\Payment\TransactionManagerInterface
+     */
+    protected function getTransactionManager()
+    {
+        $transactionManager = $this->getMock('Sonata\Component\Payment\TransactionManagerInterface');
+
+        $transactionManager->expects($this->once())
+            ->method('create')
+            ->will($this->returnValue(new Transaction()));
+
+        return $transactionManager;
+    }
+
+    /**
+     * @return \Sonata\Component\Payment\Debug\DebugPayment
+     */
+    protected function getDebugPayment()
+    {
+        $router = $this->getMock('Symfony\Component\Routing\RouterInterface');
+
+        $client = $this->getMock('Buzz\Client\ClientInterface');
+
+        $browser = new Browser($client);
+
+        $payment = new DebugPayment($router, $browser);
+
+        return $payment;
+    }
+
+    /**
+     * @return \Sonata\Component\Order\OrderInterface
+     */
+    public function getOrder()
+    {
+        $date = new \DateTime();
+        $date->setTimeStamp(strtotime('30/11/1981'));
+        $date->setTimezone(new \DateTimeZone('Europe/Paris'));
+
+        $order = new DebugPaymentTest_Order();
+        $order->setCreatedAt($date);
+
+        return $order;
+    }
+}
