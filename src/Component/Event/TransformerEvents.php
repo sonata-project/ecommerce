dd6118d3ef8859803e0d9624cef31a3fd50b502f
Vincent Composieux <composieux@ekino.com>
2014-04-01 11:31:45 +0200

    Typo fix (uppercase)
    Fix to lowercase

--- src/Component/Event/TransformerEvents.php
+++ src/Component/Event/TransformerEvents.php
@@ -20,10 +20,10 @@ namespace Sonata\Component\Event;
  */
 final class TransformerEvents
 {
-    const PRE_BASKET_TO_ORDER_TRANSFORM   = "sonata.ecommerce.pre_bASKET_TO_ORDER_transform";
-    const POST_BASKET_TO_ORDER_TRANSFORM  = "sonata.ecommerce.pre_bASKET_TO_ORDER_transform";
-    const PRE_ORDER_TO_BASKET_TRANSFORM   = "sonata.ecommerce.pre_oRDER_TO_BASKET_transform";
-    const POST_ORDER_TO_BASKET_TRANSFORM  = "sonata.ecommerce.pre_oRDER_TO_BASKET_transform";
-    const PRE_ORDER_TO_INVOICE_TRANSFORM  = "sonata.ecommerce.pre_oRDER_TO_INVOICE_transform";
-    const POST_ORDER_TO_INVOICE_TRANSFORM = "sonata.ecommerce.pre_oRDER_TO_INVOICE_transform";
+    const PRE_BASKET_TO_ORDER_TRANSFORM   = "sonata.ecommerce.pre_basket_to_order_transform";
+    const POST_BASKET_TO_ORDER_TRANSFORM  = "sonata.ecommerce.pre_basket_to_order_transform";
+    const PRE_ORDER_TO_BASKET_TRANSFORM   = "sonata.ecommerce.pre_order_to_basket_transform";
+    const POST_ORDER_TO_BASKET_TRANSFORM  = "sonata.ecommerce.pre_order_to_basket_transform";
+    const PRE_ORDER_TO_INVOICE_TRANSFORM  = "sonata.ecommerce.pre_order_to_invoice_transform";
+    const POST_ORDER_TO_INVOICE_TRANSFORM = "sonata.ecommerce.pre_order_to_invoice_transform";
 }
