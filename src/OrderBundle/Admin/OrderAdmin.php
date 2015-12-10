9832f2ecdb9e43113e9d76006b05bad93ffb260a
chus <jesusgarciatudela@gmail.com>
2015-12-10 11:19:40 +0100

    Update OrderAdmin.php

--- src/OrderBundle/Admin/OrderAdmin.php
+++ src/OrderBundle/Admin/OrderAdmin.php
@@ -209,12 +209,12 @@ class OrderAdmin extends Admin
 
         if (null === $invoice) {
             $menu->addChild(
-                $this->trans('sonata.order.sidemenu.link_oRDER_TO_INVOICE_generate', array(), 'SonataOrderBundle'),
+                $this->trans('sonata.order.sidemenu.link_order_to_invoice_generate', array(), 'SonataOrderBundle'),
                 array('uri' => $admin->generateUrl('generateInvoice', array('id' => $id)))
             );
         } else {
             $menu->addChild(
-                $this->trans('sonata.order.sidemenu.link_oRDER_TO_INVOICE_edit', array(), 'SonataOrderBundle'),
+                $this->trans('sonata.order.sidemenu.link_order_to_invoice_edit', array(), 'SonataOrderBundle'),
                 array('uri' => $this->getConfigurationPool()->getAdminByAdminCode('sonata.invoice.admin.invoice')->generateUrl('edit', array('id' => $invoice->getId())))
             );
         }
