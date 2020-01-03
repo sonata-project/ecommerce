4c5996cbf3f69e6ff1a249949c757b36d74f515d
Wojciech Błoszyk <wbloszyk@gmail.com>
2020-01-04 00:01:49 +0100

    Fix and add test for ProfileMenuBlockService

---
+++ tests/CustomerBundle/Controller/DashboardControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\CustomerBundle\Tests\Controller;
+
+use PHPUnit\Framework\TestCase;
+use Sonata\CustomerBundle\Controller\DashboardController;
+
+class DashboardControllerTest extends TestCase
+{
+    /**
+     * @var DashboardController
+     */
+    private $controller;
+
+    protected function setUp()
+    {
+        $this->controller = new DashboardController();
+    }
+
+    public function testItIsInstantiable(): void
+    {
+        $this->assertNotNull($this->controller);
+    }
+}
---
+++ tests/CustomerBundle/Twig/GlobalVariablesTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\CustomerBundle\Tests\Twig\Extension;
+
+use PHPUnit\Framework\TestCase;
+use Sonata\CustomerBundle\Twig\GlobalVariables;
+
+/**
+ * @author Wojciech Błoszyk <wbloszyk@gmail.com>
+ */
+class GlobalVariablesTest extends TestCase
+{
+    /**
+     * @var GlobalVariables
+     */
+    private $globalVariables;
+
+    protected function setUp()
+    {
+        $this->globalVariables = new GlobalVariables('SonataCustomerBundle:Profile:action.html.twig');
+    }
+
+    public function testGetProfileTemplate(): void
+    {
+        $this->assertSame('SonataCustomerBundle:Profile:action.html.twig', $this->globalVariables->getProfileTemplate());
+    }
+}
