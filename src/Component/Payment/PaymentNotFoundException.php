0d89cefc5d11b1a49c8ab6557bd4a1f9c3e1056f
Sullivan SENECHAL <soullivaneuh@gmail.com>
2020-10-11 18:09:48 +0200

    Applied fixes from FlintCI

--- src/Component/Payment/PaymentNotFoundException.php
+++ src/Component/Payment/PaymentNotFoundException.php
@@ -13,19 +13,17 @@ declare(strict_types=1);
 
 namespace Sonata\Component\Payment;
 
-use Exception;
-
 /**
  * @author Hugo Briand <briand@ekino.com>
  */
 class PaymentNotFoundException extends \InvalidArgumentException
 {
     /**
-     * @param string    $bankCode
-     * @param int       $code
-     * @param Exception $previous
+     * @param string     $bankCode
+     * @param int        $code
+     * @param \Exception $previous
      */
-    public function __construct($bankCode, $code = 0, ?Exception $previous = null)
+    public function __construct($bankCode, $code = 0, ?\Exception $previous = null)
     {
         $message = sprintf("Payment method with code '%s' was not found", $bankCode);
         parent::__construct($message, $code, $previous);
